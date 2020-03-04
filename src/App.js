d489e5550ba2f5f70b6f8e0e6c88b0c34410bb28
Damien <mail@damientimewell.com>
2020-03-04 12:52:45 +0100

    Example frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+
+const API_URL = 'https://express-image-uploads.herokuapp.com/pets'
 
 export const App = () => {
+  const fileInput = useRef()
+  const [name, setName] = useState('')
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('image', fileInput.current.files[0])
+    formData.append('name', name)
+
+    fetch(API_URL, { method: 'POST', body: formData })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+      })
+  }
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <label>
+        Pet Image
+        <input type="file" ref={fileInput} />
+      </label>
+
+      <label>
+        Pet name
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+
+      <button type="submit">
+        Submit
+      </button>
+    </form>
   )
 }
